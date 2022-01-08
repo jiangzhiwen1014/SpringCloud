ecb17c9ed8cc7895c89998625c239175997f668f
jiangzhiwen <205420057@qq.com>
2022-01-08 17:40:42 +0800

    feat:init project

---
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -0,0 +1,73 @@
+package com.atguigu.springcloud.controller;
+
+import com.atguigu.springcloud.service.PaymentService;
+import com.atguigu.springcloud.entities.CommonResult;
+import com.atguigu.springcloud.entities.Payment;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * (Payment)表控制层
+ *
+ * @author makejava
+ * @since 2020-03-06 14:22:26
+ */
+@RestController
+@RequestMapping("payment")
+public class PaymentController {
+    /**
+     * 服务对象
+     */
+    @Resource
+    private PaymentService paymentService;
+
+    @Value("${server.port}")
+    private String serverPort;
+
+
+    /**
+     * 通过主键查询单条数据
+     *
+     * @param id 主键
+     * @return 单条数据
+     */
+    @GetMapping("get/{id}")
+    public CommonResult<Payment> selectOne(@PathVariable("id") Long id) {
+        Payment payment = this.paymentService.queryById(id);
+
+        return new CommonResult<Payment>(200, "select success 8001!", payment);
+    }
+
+    @PostMapping("create")
+    public CommonResult create(@RequestBody Payment payment) {
+        Payment insert = this.paymentService.insert(payment);
+        System.out.println(insert);
+        System.out.println("1234567890");
+        return new CommonResult(200, "insert success", insert);
+    }
+
+
+    @GetMapping("lb")
+    public String getPaymentLB() {
+        return serverPort;
+    }
+
+    @GetMapping("feign/timeout")
+    public String getFeignTimeOut() {
+        try {
+            TimeUnit.SECONDS.sleep(3);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return serverPort;
+    }
+
+    @GetMapping("zipkin")
+    public String paymentZipkin() {
+        return "hi,i`am paymentzipkin server fall back.welcome to atguigu.hahahahahhahahah";
+    }
+}
